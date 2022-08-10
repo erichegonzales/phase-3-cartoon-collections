3f414c9210754993a8f5efc24636acda851eb127
Eriche Gonzales <erichelouise@gmail.com>
2022-08-10 16:00:34 -0400

    commit tree 5c77bfb304d60a802aad268e2a001b826c3e3de8

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,49 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+# def roll_call_dwarves(dwarves)
+#   dwarves.map.with_index do |dwarf, index|
+#     puts "#{index + 1}. #{dwarf}"
+#   end
+# end
+
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.map do |call|
+#     call.capitalize + "!"
+#   end
+# end
+
+# def long_planeteer_calls(planeteer_calls)
+#   planeteer_calls.find do |call|
+#     return true if call.length > 4
+#   end
+#   return false
+# end
+
+# def find_the_cheese(ingredients)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheese_types.each do |cheese|
+#     return cheese if ingredients.include?(cheese)
+#   end
+#   return nil
+# end
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |ingredient| 
+    cheese_types.include?(ingredient)
+  end
 end
